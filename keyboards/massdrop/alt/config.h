db197f566318259769b6835a6872c2f69a2f2480
mwyrick <mjw@axiosengineering.com>
2020-01-30 13:45:22 -0500

    User Custom for wyricks Alt
    # Conflicts:
    #	keyboards/massdrop/alt/rules.mk

--- keyboards/massdrop/alt/config.h
+++ keyboards/massdrop/alt/config.h
@@ -100,7 +100,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define LOCKING_RESYNC_ENABLE
 
 /* Force boot in NKRO mode */
-//#define FORCE_NKRO
+#define FORCE_NKRO
 
 /*
  * Feature disable options
