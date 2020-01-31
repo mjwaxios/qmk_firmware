b37b44d879e62b6db7cfbf3bcbb1bdb178ccfd7b
mwyrick <mjw@axiosengineering.com>
2020-01-31 14:29:42 -0500

    Added L_MIKE to fn-BackSpace

--- keyboards/massdrop/alt/keymaps/mike6/keymap.c
+++ keyboards/massdrop/alt/keymaps/mike6/keymap.c
@@ -1,7 +1,34 @@
 #include QMK_KEYBOARD_H
 
+enum alt_keycodes {
+    L_MIKE = SAFE_RANGE
+
+};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_65_ansi_blocker(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC, KC_DEL, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_PGUP, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_UP, KC_PGDN, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT),
-	[1] = LAYOUT_65_ansi_blocker(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_MNXT, KC_MUTE, BL_BRTG, BL_INC, RGB_VAI, RGB_MOD, RGB_SPI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_SLCK, KC_PAUS, KC_MPRV, KC_MSTP, BL_TOGG, BL_DEC, RGB_VAD, RGB_RMOD, RGB_SPD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DF(0), KC_TRNS, KC_VOLU, KC_TRNS, RGB_M_K, RGB_TOG, RGB_M_SW, KC_NO, RESET, MAGIC_TOGGLE_NKRO, MAGIC_HOST_NKRO, MAGIC_UNHOST_NKRO, DF(2), DF(1), KC_TRNS, KC_PGUP, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_PGDN, KC_END),
+	[1] = LAYOUT_65_ansi_blocker(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, L_MIKE, KC_MUTE, BL_BRTG, BL_INC, RGB_VAI, RGB_MOD, RGB_SPI, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_SLCK, KC_PAUS, KC_MPRV, KC_MSTP, BL_TOGG, BL_DEC, RGB_VAD, RGB_RMOD, RGB_SPD, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, DF(0), KC_TRNS, KC_VOLU, KC_TRNS, RGB_M_K, RGB_TOG, RGB_M_SW, KC_NO, RESET, MAGIC_TOGGLE_NKRO, MAGIC_HOST_NKRO, MAGIC_UNHOST_NKRO, DF(2), DF(1), KC_TRNS, KC_PGUP, KC_VOLD, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_PGDN, KC_END),
 	[2] = LAYOUT_65_ansi_blocker(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_LBRC, KC_RBRC, KC_BSPC, DF(0), KC_TAB, KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_SLSH, KC_EQL, KC_BSLS, DF(1), KC_CAPS, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_MINS, KC_ENT, KC_NO, KC_LSFT, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, KC_RSFT, KC_UP, KC_NO, KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT)
 };
+
+// Runs just one time when the keyboard initializes.
+void matrix_init_user(void) {
+};
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) {
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case L_MIKE:
+          if (record->event.pressed) {
+            SEND_STRING("This is Mike's KeyBoard");
+          } else {
+            SEND_STRING("...");
+          }
+          return false;
+      break;
+        default:
+            return true; //Process all other keycodes normally
+    }
+}
